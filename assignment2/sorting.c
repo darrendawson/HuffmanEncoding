bbd679459e87ee7f5c8712f8a01915ec026cbb9b
Darren Dawson <dwdawson@ucsc.edu>
2017-05-03 23:14:01 -0700

    Testing harness complete

--- assignment2/sorting.c
+++ assignment2/sorting.c
@@ -61,6 +61,46 @@ void parseArguments(int argc, char *argv[], int *sorts,
   }  
 }
 
+//---createNumArray------------------------------------------------------
+
+// creates array of random numbers of certain size
+void createNumArray(uint32_t **numArray, uint32_t sizeOfArray)
+{
+  int bitMask = 0x00ffffff; // limit numbers to 24 bits
+
+  // allocate in memory
+  *numArray = (uint32_t*)calloc(sizeOfArray, sizeof(uint32_t));
+
+  
+  // set ints in array to random numbers
+  for (uint32_t i=0; i < sizeOfArray; i++)
+  {
+    (*numArray)[i] = (uint32_t)(rand() & bitMask);
+  }
+}
+
+//---printArray----------------------------------------------------------
+
+// prints the first n numbers in an array
+void printArray(uint32_t **numArray, uint32_t numsToPrint)
+{
+  int newLine = 0;
+  //print 7 numbers in each row
+  for(uint32_t i=0; i < numsToPrint; i++)
+  {
+    printf("%10d", (*numArray)[i]);
+    if (newLine == 6)
+    {
+      printf("\n");
+      newLine = 0;
+    }
+    else
+    {
+      newLine++;
+    }
+  }
+  printf("\n");
+}
 
 //---MAIN----------------------------------------------------------------
 int main(int argc, char *argv[])
@@ -68,22 +108,26 @@ int main(int argc, char *argv[])
   uint32_t sizeOfArray = 100;
   uint32_t randomSeed = 8062022;
   uint32_t numsToPrint = 100;
-
   uint32_t *sizeOfArrayP = &sizeOfArray;
   uint32_t *randomSeedP = &randomSeed;
   uint32_t *numsToPrintP = &numsToPrint;
 
-  int sorts[5] = {0, 0, 0, 0, 0};
+  int sorts[5] = {0, 0, 0, 0, 0}; // determines what sorts to run
+  uint32_t *numArray; // array of random numbers to be sorted
 
-  // Use arguments to set up array
+  // Use command line arguments to set up array
   parseArguments(argc, argv, sorts, sizeOfArrayP,
 		 randomSeedP, numsToPrintP);
-
   
   //Create array of random numbers
+  srand(randomSeed);
+  createNumArray(&numArray, sizeOfArray);
 
-  printf("Size of Array: %d\nRandom Seed: %d\n", sizeOfArray, randomSeed);
-  printf("nums to print: %d", numsToPrint);
+  printArray(&numArray, 30);
+  
+  //printf("Size of Array: %d\nRandom Seed: %d\n", sizeOfArray, randomSeed);
+  //printf("nums to print: %d", numsToPrint);
 
+  free(numArray);
   return 0;
 }
